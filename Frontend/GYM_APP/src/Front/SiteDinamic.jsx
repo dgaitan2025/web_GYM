8ea1360d5b1c4b3a71a2f3bb22b54d2605b6c4b9
Josue Anti <josueanti@gmail.com>
2025-07-31 22:12:26 -0600

    Proyecto Gym Formulario Clientes Agregado

--- Frontend/GYM_APP/src/Front/SiteDinamic.jsx
+++ Frontend/GYM_APP/src/Front/SiteDinamic.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import { useNavigate, Outlet } from "react-router-dom";
 import "./SiteDinamic.css";
 
+
+
 export default function SiteDinamic() {
   const [menuOpen, setMenuOpen] = useState(false);
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
